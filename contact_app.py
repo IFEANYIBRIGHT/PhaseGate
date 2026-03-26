8404044e65062bdacb0728f1779dc4993f7e029c
Ifeanyi bright <brightifeanyi799@gmail.com>
2026-03-26 12:30:35 +0000

    Initial commit

---
+++ contact_app.py
@@ -0,0 +1,33 @@
+def add_Contact(first_name,last_name,phone_number):
+
+    add_contact_needs = []
+    
+    first_name = "Ifeanyi"
+    
+    add_contact_needs.append(first_name)
+    
+    last_name = "bright"
+    
+    add_contact_needs.append(last_name)
+    
+    phone_number = "08131913381"
+    
+    add_contact_needs.append(phone_number)
+    
+    return add_contact_needs
+    
+    
+    
+def remove_contact(contact):
+
+    add_contact_needs = []
+       
+    add_contact_needs.append(contact)
+                 
+    add_contact_needs.remove(contact)
+    
+    return add_contact_needs
+    
+    
+    
+    
